d84bb044b96894f285ecf67192025b93e8e0d618
gsbakshi <greaser.bakshi@gmail.com>
2021-09-20 14:41:46 +0530

    Added textbox component, styled content container

--- metaface-frontend/src/components/input-image-url/input-image-url.component.jsx
+++ metaface-frontend/src/components/input-image-url/input-image-url.component.jsx
@@ -5,35 +5,19 @@ import './input-image-url.styles.scss';
 
 const InputImageUrl = ({ onInputChange, onEnter, onSubmit }) => (
     <div className='input-image-container'>
-        {/* <input
-            className='url-input'
-            type='text'
+        <TextBox
             placeholder='Paste image link'
-            onChange={ onInputChange }
-            onKeyDown={ onEnter }
-            // value={ input }
-            // autoFocus={ true }
-            />
-        <button
-            className='detect'
-            onClick={ onSubmit }
-        >
-            <i class='fa fa-angle-right' />
-        </button> */}
-    <TextBox
-        className='textbox'
-        placeholder='Paste image link'
-        component={
-            <button
-                className='detect'
-                onClick={ onSubmit }
-                onChange={ onInputChange }
-                onKeyDown={ onEnter }
-            >
-                <i class='fa fa-angle-right' />
-            </button>
-        }
-    />
+            component={
+                <button
+                    className='detect'
+                    onClick={ onSubmit }
+                    onChange={ onInputChange }
+                    onKeyDown={ onEnter }
+                >
+                    <i class='fa fa-angle-right' />
+                </button>
+            }
+        />
     </div>
 );
 
