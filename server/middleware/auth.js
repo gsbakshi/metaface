30dc94b006a7b7a8df80039ff3c32af8c2926bac
gsbakshi <greaser.bakshi@gmail.com>
2021-10-12 19:25:15 +0530

    Setting up server middleware for auth and validation

--- server/middleware/auth.js
+++ server/middleware/auth.js
@@ -1,11 +1,12 @@
-const redisClient = require("./signin").redisClient;
+const redisClient = require("./redis");
 
 const requireAuth = (req, res, next) => {
   const { authorization } = req.headers;
   if (!authorization) {
     return res.status(401).send("Unauthorized");
   }
-  return redisClient.get(authorization, (err, reply) => {
+  const token = authorization.split(" ")[1];
+  return redisClient.get(token, (err, reply) => {
     if (err || !reply) {
       return res.status(401).send("Unauthorized");
     }
