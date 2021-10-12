213f2417887742f05f21ea2182a846a87d0a6fb9
gsbakshi <greaser.bakshi@gmail.com>
2021-10-12 19:29:36 +0530

    Updated client to manage sessions with jwt

--- server/controllers/signin.js
+++ server/controllers/signin.js
@@ -1,13 +1,44 @@
-const { validate } = require("./validate");
+const jwt = require("jsonwebtoken");
 
-const handleSignin = (db, bcrypt) => (req, res) => {
+const redisClient = require("../middleware/redis");
+const { validate } = require("../middleware/validate");
+
+const signToken = (username) => {
+  const jwtPayload = { username };
+  return jwt.sign(jwtPayload, process.env.JWT_SECRET, { expiresIn: "2 days" });
+};
+
+const setToken = (key, value) => Promise.resolve(redisClient.set(key, value));
+
+// const setToken = (key, value) => (resolve, reject) => {
+//   redisClient.set(key, value, (error, result) => {
+//     if (error) {
+//       reject(error);
+//     }
+//     resolve(result);
+//   });
+// };
+
+const createSession = async (user) => {
+  const { email, id } = user;
+  const token = signToken(email);
+  try {
+    setToken(token, id);
+    return { success: "true", userId: id, token};
+  } catch (message) {
+    return console.log(message);
+  }
+};
+
+const handleSignin = (db, bcrypt, req, res) => {
   const { email, password } = req.body;
   if (!email || !password) {
-    return res.status(400).json("incorrect form submission");
+    return Promise.reject("incorrect form submission");
   }
   const validatedEmail = validate(email);
   const validatedPassword = validate(password);
-  db.select("email", "hash")
+  return db
+    .select("email", "hash")
     .from("login")
     .where("email", "=", validatedEmail)
     .then((data) => {
@@ -17,17 +48,43 @@ const handleSignin = (db, bcrypt) => (req, res) => {
           .select("*")
           .from("users")
           .where("email", "=", email)
-          .then((user) => {
-            res.json(user[0]);
-          })
+          .then((user) => user[0])
           .catch((err) => res.status(400).json("unable to get user"));
       } else {
-        res.status(400).json("wrong credentials");
+        return Promise.reject("wrong credentials");
       }
     })
-    .catch((err) => res.status(400).json("wrong credentials"));
+    .catch((error) => error);
+};
+
+const getAuthTokenId = (req, res) => {
+  const { authorization } = req.headers;
+  if (!authorization) {
+    return res.status(401).send("Unauthorized");
+  }
+  const token = authorization.split(" ")[1];
+  return redisClient.get(token, (error, reply) => {
+    if (error || !reply) {
+      return res.status(401).send("Unauthorized");
+    }
+    return res.json({ id: reply });
+  });
+};
+
+const signinAuthentication = (db, bcrypt) => (req, res) => {
+  const { authorization } = req.headers;
+  return authorization
+    ? getAuthTokenId(req, res)
+    : handleSignin(db, bcrypt, req, res)
+        .then((data) => {
+          return data.id && data.email
+            ? createSession(data)
+            : Promise.reject("invalid credentials");
+        })
+        .then((session) => res.json(session))
+        .catch((error) => res.status(400).json(error));
 };
 
 module.exports = {
-  handleSignin: handleSignin,
+  signinAuthentication: signinAuthentication,
 };
