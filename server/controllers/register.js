22118a59fbb6574d5e7501bca57174a5407e83e2
gsbakshi <greaser.bakshi@gmail.com>
2021-10-12 22:44:06 +0530

    Updated registration to use redis

--- server/controllers/register.js
+++ server/controllers/register.js
@@ -1,4 +1,35 @@
+const jwt = require("jsonwebtoken");
+
+const redisClient = require("../middleware/redis");
 const { validate } = require("../middleware/validate");
+
+const signToken = (username) => {
+  const jwtPayload = { username };
+  return jwt.sign(jwtPayload, process.env.JWT_SECRET, { expiresIn: "2 days" });
+};
+
+const setToken = (key, value) => Promise.resolve(redisClient.set(key, value));
+
+// TODO fix promise with resolve and reject
+// const setToken = (key, value) => (resolve, reject) => {
+//   redisClient.set(key, value, (error, result) => {
+//     if (error) {
+//       reject(error);
+//     }
+//     resolve(result);
+//   });
+// };
+
+const createSession = async (user) => {
+  const { email, id } = user;
+  const token = signToken(email);
+  try {
+    setToken(token, id);
+    return { success: "true", userId: id, token };
+  } catch (message) {
+    return console.log(message);
+  }
+};
 
 const handleRegister = (db, bcrypt) => (req, res) => {
   const { email, name, password } = req.body;
@@ -10,7 +41,7 @@ const handleRegister = (db, bcrypt) => (req, res) => {
   const validatedPassword = validate(password);
   const hash = bcrypt.hashSync(validatedPassword);
   db.transaction((trx) => {
-    trx
+    return trx
       .insert({
         hash: hash,
         email: validatedEmail,
@@ -25,13 +56,18 @@ const handleRegister = (db, bcrypt) => (req, res) => {
             name: validatedName,
             joined: new Date(),
           })
-          .then((user) => {
-            res.json(user[0]);
-          });
+          .then((user) => user[0]);
       })
       .then(trx.commit)
       .catch(trx.rollback);
-  }).catch((err) => res.status(400).json("unable to register"));
+  })
+    .then((data) => {
+      return data.id && data.email
+        ? createSession(data)
+        : Promise.reject("invalid credentials");
+    })
+    .then((session) => res.json(session))
+    .catch((err) => res.status(400).json("unable to register"));
 };
 
 module.exports = {
