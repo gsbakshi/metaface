913446012aebe7d0d52de77274aca27067571f96
gsbakshi <greaser.bakshi@gmail.com>
2021-10-12 19:27:22 +0530

    Updating profile functions

--- server/controllers/profile.js
+++ server/controllers/profile.js
@@ -13,6 +13,23 @@ const handleProfileGet = (db) => (req, res) => {
     .catch((err) => res.status(400).json("error getting user"));
 };
 
+const handleProfileUpdate = (db) => (req, res) => {
+  const { id } = req.params;
+  const { name } = req.body.formInput;
+  db("users")
+    .where({ id })
+    .update({ name: name })
+    .then((resp) => {
+      if (resp) {
+        res.json("success");
+      } else {
+        res.status(400).json("Not found");
+      }
+    })
+    .catch((err) => res.status(400).json("error updating user"));
+};
+
 module.exports = {
   handleProfileGet,
+  handleProfileUpdate,
 };
